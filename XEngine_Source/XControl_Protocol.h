966ba5f24b7a98ae312ffc819722599b782cf812
qyt <486179@qq.com>
2021-09-13 13:11:08 +0800

    fixed:Module copy script is missing module added:XRPC Protocol

--- XEngine_Source/XControl_Protocol.h
+++ XEngine_Source/XControl_Protocol.h
@@ -9,6 +9,33 @@
 //    Purpose:     控制协议
 //    History:
 *********************************************************************/
+//////////////////////////////////////////////////////////////////////////
+//                        导出的枚举型
+//////////////////////////////////////////////////////////////////////////
+//类型
+typedef enum
+{
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_UNKNOW = 0,                           //未知类型,请求端可以为0,不指定
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_VOID = 1,                             //无类型,使用此类型作为返回值,服务器将不返回任何东西
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_BOOL = 2,                             //逻辑型
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_INT = 3,                              //整数型
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_UINT = 4,                             //无符号整数型
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_LONGINT = 5,                          //长整数型
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_ULONG = 6,                            //无符号长整数型
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_FLOAT = 7,                            //浮点型
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_DOUBLE = 8,                           //双精度浮点型
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_STRING = 9,                           //字符串
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE_BIN = 10                              //二进制数据
+}ENUM_PROTOCOL_XRPC_PARAMETE_TYPE, * LPENUM_PROTOCOL_XRPC_PARAMETE_TYPE;
+//////////////////////////////////////////////////////////////////////////
+//                        导出的数据结构
+//////////////////////////////////////////////////////////////////////////
+typedef struct
+{
+	ENUM_PROTOCOL_XRPC_PARAMETE_TYPE enXRPC_ParameteType;                  //参数类型
+	int nParameteLen;                                                     //参数大小,数值相关类型这个值可以为0
+	LPVOID lParameteValue;                                                //自定义参数值
+}PROTOCOL_XRPCPARAMETE, * LPPROTOCOL_XRPCPARAMETE;
 ///////////////////////////////////////////////////////////////////////////
 //                          导出的数据定义
 ///////////////////////////////////////////////////////////////////////////
