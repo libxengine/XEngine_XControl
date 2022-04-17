6d8dac99be95fdc42c9b06ed7f70cda5166307f7
qyt <486179@qq.com>
2022-04-17 18:08:48 +0800

    modify:vs copyfile

--- XEngine_Source/VSCopy-Debue.bat
+++ XEngine_Source/VSCopy-Debue.bat
@@ -3,6 +3,7 @@ copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_Algorithm.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_Core.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_ManagePool.dll" "./"
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_OPenSsl.dll" "./"
+copy /y "D:\XEngine\XEngine_SourceCode\Debug\XEngine_NetXApi.dll" "./"
 
 copy /y "D:\XEngine\XEngine_SourceCode\Debug\XClient_Socket.dll" "./"
 
